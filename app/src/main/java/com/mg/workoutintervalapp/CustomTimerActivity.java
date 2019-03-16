66778c115c536de3761cf7814836ec1db4de344d
Michael Glaser <mglaser001@gmail.com>
2019-03-16 12:11:24 -0700

    -Clean up code

--- app/src/main/java/com/mg/workoutintervalapp/CustomTimerActivity.java
+++ app/src/main/java/com/mg/workoutintervalapp/CustomTimerActivity.java
@@ -1,10 +1,8 @@
 package com.mg.workoutintervalapp;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.CountDownTimer;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,18 +14,16 @@ import com.mg.TransferObjects.CustomCircuitTO;
 import com.mg.TransferObjects.IntervalTo;
 
 public class CustomTimerActivity extends AppCompatActivity {
+
     private MediaPlayer bell;
     private TextView countdownText, intervalNameText, titleText, repText;
     private Button backToMenuButton, nextWorkoutButton;
     private CountDownTimer countDownTimer, countDownTimer2;
     private long timeLeftInMilliseconds;
     private long threeSecondPrepare = 3000;
-    private long timeToDecrement_Rest;
-    private long timeToDecrement;
-    private int intervalsLeft, pos;
-    private int intervalsLeftforReset;
+    private int pos;
+
     RelativeLayout timerLayout;
-    private boolean stopTime = false;
     private CustomCircuitTO customCircuitTO;
 
     @Override
@@ -35,42 +31,20 @@ public class CustomTimerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_timer);
 
-        timerLayout = findViewById(R.id.customTimerLayout);
+        initializeActivityItems();
 
-        bell = MediaPlayer.create(this,R.raw.boxingbell);
-        titleText = findViewById(R.id.CustomCircuitNameTV);
-        intervalNameText = findViewById(R.id.CustomCircuitIntervalNameTV);
-        countdownText = findViewById(R.id.CustomCircuitTimerTV);
-        repText = findViewById(R.id.CustomCircuitTimedRepTV);
-        backToMenuButton = findViewById(R.id.CustomCircuitDoneBTN);
-        nextWorkoutButton = findViewById(R.id.CustomCircuitNextBTN);
-        nextWorkoutButton.setVisibility(View.GONE);
-        repText.setVisibility(View.GONE);
-
-        if(getIntent().hasExtra("timeToDecrement")){
-            timeToDecrement = getIntent().getExtras().getLong("timeToDecrement");
-        }
         if(getIntent().hasExtra("customCircuitTO")){
             customCircuitTO = (CustomCircuitTO) getIntent().getSerializableExtra("customCircuitTO");
         }
-        if(getIntent().hasExtra("intervalsLeft")){
-            intervalsLeft = getIntent().getExtras().getInt("intervalsLeft");
-        }
-        if(getIntent().hasExtra("restTimeToDecrement")){
-            timeToDecrement_Rest = getIntent().getExtras().getLong("restTimeToDecrement");
-        }
-        //Custom Timer Stuff
+
+        //Custom Timer Start
         titleText.setText(customCircuitTO.getName());
-
-        intervalsLeftforReset = intervalsLeft;
-
         startInterval();
 
 
         backToMenuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                stopTime = true;
                 resetTimers();
                 finish();
             }
@@ -80,7 +54,7 @@ public class CustomTimerActivity extends AppCompatActivity {
     private void startInterval() {
         updateTimerText("Get Ready");
         updateIntervalTitle("");
-        stopTime = false;
+
         timerLayout.setBackgroundColor(Color.parseColor("#7D8E32"));
 
         countDownTimer2 = new CountDownTimer(threeSecondPrepare+200, 1000) {
@@ -103,7 +77,7 @@ public class CustomTimerActivity extends AppCompatActivity {
         this.pos = position;
         updateIntervalTitle(intervalTo.getIntervalName());
         repText.setVisibility(View.GONE);
-        //No Timer Running
+        //No Timer Running, Type = reps
         if(intervalTo.getIntervalTime().equalsIgnoreCase("#NOTIME")){
             updateTimerText(intervalTo.getIntervalReps() + " REPS");
             nextWorkoutButton.setVisibility(View.VISIBLE);
@@ -114,13 +88,14 @@ public class CustomTimerActivity extends AppCompatActivity {
                     if(pos < customCircuitTO.getintervalToList().size()){
                         startTimer(customCircuitTO.getintervalToList().get(pos), pos);
                     }else{
+                        nextWorkoutButton.setVisibility(View.GONE);
                         updateTimerText("DONE!");
                     }
                 }
             });
 
         }
-        //Timer Running
+        //Timer Running Type = timedReps, timed, rest
         else{
             if(!intervalTo.getIntervalReps().contains("#NOREPS")){
                 repText.setVisibility(View.VISIBLE);
@@ -181,11 +156,24 @@ public class CustomTimerActivity extends AppCompatActivity {
             countDownTimer.cancel();
         }
     }
-    public void updateTimerText(String text) {
+    private void updateTimerText(String text) {
         countdownText.setText(text);
     }
 
     private void updateIntervalTitle(String intervalName) {
         intervalNameText.setText(intervalName);
+    }
+    private void initializeActivityItems(){
+        timerLayout = findViewById(R.id.customTimerLayout);
+
+        bell = MediaPlayer.create(this,R.raw.boxingbell);
+        titleText = findViewById(R.id.CustomCircuitNameTV);
+        intervalNameText = findViewById(R.id.CustomCircuitIntervalNameTV);
+        countdownText = findViewById(R.id.CustomCircuitTimerTV);
+        repText = findViewById(R.id.CustomCircuitTimedRepTV);
+        backToMenuButton = findViewById(R.id.CustomCircuitDoneBTN);
+        nextWorkoutButton = findViewById(R.id.CustomCircuitNextBTN);
+        nextWorkoutButton.setVisibility(View.GONE);
+        repText.setVisibility(View.GONE);
     }
 }
